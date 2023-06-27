5355cf27afc9baafdebb2b857b97de7a45d24cf3
Michael Khanda <michaelkhanda006@gmail.com>
2023-06-27 19:05:25 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*Write a program that can prompt the user for details of two arrays (size 5 each) and then inform the user if the arrays have the same data*/
+
+
+using System;
+
+namespace Arrayssearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int [5];
+            int[] b = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter freshman Age: ");
+                a[i] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Sophomore Age");
+                b[i] = int.Parse(Console.ReadLine());
+
+                
+            }
+
+
+            for (int i = 0; i < 5; i++)
+                if (a[i] == b[i])
+            {
+                Console.WriteLine(a[i] + " and " + b[i] + " Are Similar");
+            }
+        }
+    }
+}
